55458ce23d155048958acb2f4bf0184688ffac88
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2024-01-11 10:57:58 -0300

    criando formulario de produtos

--- src/pages/Admin/Produtos/AdminFormProduto.tsx
+++ src/pages/Admin/Produtos/AdminFormProduto.tsx
@@ -1,6 +1,144 @@
+import { Box, Button, Container, FormControl, Input, InputAdornment, InputLabel, MenuItem, OutlinedInput, Paper, Select, TextField, Typography } from "@mui/material"
+import categories from '../../../data/categoriesMenu.json';
+import subcategories from '../../../data/subcategoriesMenu.json';
+import { useEffect, useState } from "react";
+
+interface Subcategory {
+    id: number
+    name: string
+    category: number
+}
+
 const AdminFormProdutos = () => {
+    const [category, setCategory] = useState("");
+    const [filteredOptions, setFilteredOptions] = useState<Subcategory[]>([])
+
+    useEffect(() => {
+        const subCategoriesFiltered = subcategories.filter(subcategory => subcategory.category === Number(category))
+        setFilteredOptions(subCategoriesFiltered);
+    }, [category])
+
+
     return (
         <>
+            <Box>
+                <Container maxWidth='lg' sx={{my: 2}}>
+                    <Paper sx={{p: 2}}>
+                        <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1}}>
+                            <Typography variant="h6" component="h1">Formulário de Produtos</Typography>
+                            <Box component="form" sx={{width: '100%', display: 'flex', flexDirection: 'column', gap: 2}}>
+                                <TextField 
+                                    label="Nome do Produto"
+                                    variant="standard"
+                                    fullWidth
+                                    margin="dense"
+                                    required
+                                />
+
+                                <TextField 
+                                    label="Descrição do Produto"
+                                    variant="standard"
+                                    fullWidth
+                                    margin="dense"
+                                />
+
+                                <TextField 
+                                    label="Link da Imagem"
+                                    variant="standard"
+                                    fullWidth
+                                    margin="dense"
+                                    required
+                                />
+
+                                <TextField 
+                                    label="Peso"
+                                    variant="standard"
+                                    fullWidth
+                                    margin="dense"
+                                    type="number"
+                                    InputProps={{
+                                        startAdornment: <InputAdornment position="start">g</InputAdornment>,
+                                      }}
+                                    helperText="(Apenas números)"
+                                />
+                                
+                                <TextField 
+                                    label="Serve"
+                                    variant="standard"
+                                    fullWidth
+                                    margin="dense"
+                                    type="number"
+                                    helperText="(Quantidade de pessoas, apenas números)"
+                                />
+
+                                <FormControl 
+                                    fullWidth 
+                                    variant="standard" 
+                                    required
+                                >
+                                    <InputLabel>Preço</InputLabel>
+                                    <Input
+                                        type="number"
+                                        startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+                                    />
+                                </FormControl>
+
+                                <FormControl
+                                    fullWidth
+                                    variant="standard"
+                                    required
+                                >
+                                    <InputLabel className="ml-3">Categoria</InputLabel>
+                                    <Select
+                                        defaultValue=""
+                                        input={<OutlinedInput label="Categoria" />}
+                                        onChange={(e) => setCategory(e.target.value)}
+                                    >
+                                        {categories.map(category => (
+                                            <MenuItem 
+                                                key={category.id}
+                                                value={category.id}
+                                            >
+                                                {category.name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+
+                                <FormControl
+                                    fullWidth
+                                    variant="standard"
+                                    required
+                                >
+                                    <InputLabel className="ml-3">Subcategoria</InputLabel>
+                                    <Select
+                                        defaultValue=""
+                                        input={<OutlinedInput label="Subcategoria" />}
+                                    >
+                                        {filteredOptions.map(subcategory => (
+                                            <MenuItem 
+                                                key={subcategory.id}
+                                                value={subcategory.id}
+                                            >
+                                                {subcategory.name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+
+                                <Button 
+                                    sx={{mt: 2}}
+                                    type="submit" 
+                                    variant="outlined"
+                                    fullWidth 
+                                >
+                                    Salvar
+                                </Button>
+                            </Box>
+                        </Box>
+                    </Paper>
+                </Container>
+            </Box>
         </>
     )
 }
